4bdef0feac626c32547585945e9217eb0fb54b99
ricardodsb <ricardojdasilva15@gmail.com>
2021-10-03 20:42:50 +0000

    changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  let Number = Math.floor(Math.random() * 12 - 0) + 1;
-  let randomnumber = (document.querySelector("#number").innerHTML = Number);
+window.onload = () => {
+  let numbers = Math.floor(Math.random() * 12 - 0) + 1;
+  let numberDiv = (document.querySelector("#number").innerHTML = numbers);
+
+  if (numbers == 11) {
+    numberDiv.innerHTML = "K";
+  } else if (numbers == 0) {
+    numberDiv.innerHTML = "A";
+  } else if (numbers == 10) {
+    numberDiv.innerHTML = "Q";
+  } else if (numbers == 9) {
+    numberDiv.innerHTML = "J";
+  }
 };
