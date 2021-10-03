26ecef873bba2600b77daf02d119d1605d2fcbf1
ricardodsb <ricardojdasilva15@gmail.com>
2021-10-03 21:44:46 +0000

    changes

--- src/app.js
+++ src/app.js
@@ -8,14 +8,4 @@ import "./assets/img/4geeks.ico";
 window.onload = () => {
   let numbers = Math.floor(Math.random() * 12 - 0) + 1;
   let numberDiv = (document.querySelector("#number").innerHTML = numbers);
-
-  if (numbers == 11) {
-    numberDiv.innerHTML = "K";
-  } else if (numbers == 0) {
-    numberDiv.innerHTML = "A";
-  } else if (numbers == 10) {
-    numberDiv.innerHTML = "Q";
-  } else if (numbers == 9) {
-    numberDiv.innerHTML = "J";
-  }
 };
