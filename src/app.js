3d2028b1b595621e29b2789a596e79f1092d068f
ricardodsb <ricardojdasilva15@gmail.com>
2021-10-04 09:29:27 +0000

    changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = () => {
-  let numbers = Math.floor(Math.random() * 12 - 0) + 1;
-  let numberDiv = (document.querySelector("#number").innerHTML = numbers);
+window.onload = function() {
+  let symbols = { 0: "♦", 1: "♥", 2: "♠", 3: "♣" };
+  let colors = { 0: "red", 1: "red", 2: "black", 3: "black" };
+  let i = Math.floor(Math.random() * 4);
+  let n = Math.floor(Math.random() * 12 - 0) + 1;
+
+  let divsymbols = document.querySelectorAll(".form");
+  divsymbols[0].innerHTML = `<span style="color:${colors[i]}">${symbols[i]}</span>`;
+  divsymbols[1].innerHTML = `<span style="color:${colors[i]}">${symbols[i]}</span>`;
+  document.querySelector(
+    ".number"
+  ).innerHTML = `<span style="color:${colors[i]}">${n}</span>`;
 };
