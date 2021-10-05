8d2806e4e382f8e91f608c570713a4943ff2c99b
ricardodsb <ricardojdasilva15@gmail.com>
2021-10-05 09:50:22 +0000

    final changes

--- src/app.js
+++ src/app.js
@@ -5,16 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+let card = document.querySelector(".card");
+let btn = document.querySelector(".mybtn");
+
+window.onload = generateCard;
+btn.addEventListener("click", generateCard);
+
+function generateCard() {
   let symbols = { 0: "♦", 1: "♥", 2: "♠", 3: "♣" };
   let colors = { 0: "red", 1: "red", 2: "black", 3: "black" };
+  let letters = { 11: "J", 12: "Q", 13: "K" };
   let i = Math.floor(Math.random() * 4);
-  let n = Math.floor(Math.random() * 12 - 0) + 1;
+  let n = Math.floor(Math.random() * 12) + 1;
 
-  let divsymbols = document.querySelectorAll(".form");
+  let divsymbols = document.querySelectorAll(".symbol");
   divsymbols[0].innerHTML = `<span style="color:${colors[i]}">${symbols[i]}</span>`;
   divsymbols[1].innerHTML = `<span style="color:${colors[i]}">${symbols[i]}</span>`;
-  document.querySelector(
-    ".number"
-  ).innerHTML = `<span style="color:${colors[i]}">${n}</span>`;
-};
+  document.querySelector(".number").innerHTML = `<span style="color:${
+    colors[i]
+  }">${n < 11 ? n : letters[n]}</span>`;
+}
