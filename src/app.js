77443c8d65460364d8e21f6aa0e0d5cd2b0b8af4
ricardodsb <ricardojdasilva15@gmail.com>
2021-10-07 17:54:00 +0000

    change on numbers

--- src/app.js
+++ src/app.js
@@ -14,14 +14,14 @@ btn.addEventListener("click", generateCard);
 function generateCard() {
   let symbols = { 0: "♦", 1: "♥", 2: "♠", 3: "♣" };
   let colors = { 0: "red", 1: "red", 2: "black", 3: "black" };
-  let letters = { 11: "J", 12: "Q", 13: "K" };
+  let letters = { 1: "A", 11: "J", 12: "Q", 13: "K" };
   let i = Math.floor(Math.random() * 4);
-  let n = Math.floor(Math.random() * 12) + 1;
+  let n = Math.floor(Math.random() * 13 - 0) + 1;
 
   let divsymbols = document.querySelectorAll(".symbol");
   divsymbols[0].innerHTML = `<span style="color:${colors[i]}">${symbols[i]}</span>`;
   divsymbols[1].innerHTML = `<span style="color:${colors[i]}">${symbols[i]}</span>`;
   document.querySelector(".number").innerHTML = `<span style="color:${
     colors[i]
-  }">${n < 11 ? n : letters[n]}</span>`;
+  }">${n < 11 && n > 1 ? n : letters[n]}</span>`;
 }
